31d213de4994b85574fefdd7a6daadbde0cdfa48
shenhuihuang <1605312624@qq.com>
2020-11-12 16:36:10 +0800

    新配置的项目文件

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'blog'
     'xadmin',
     'crispy_forms',
+    'reversion',
 ]
 
 MIDDLEWARE = [
